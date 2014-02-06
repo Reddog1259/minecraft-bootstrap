73d424e08502a2cce2d644615c9190e3d9ef6ad7
Reddog1259 <nicoq1259@gmail.com>
2014-02-05 14:37:06 -1000

    Firs

---
+++ src/main/java/net/minecraft/bootstrap/BootstrapConstants.java
@@ -0,0 +1,8 @@
+package net.minecraft.bootstrap;
+
+public class BootstrapConstants {
+    public static final String MD5_FILE = "http://maohicraft.crystal-serv.com/launcherbeta/md5.txt"; //Lien vers le fichier md5.txt situé dans votre dossier launcher sur votre serveur FTP.
+    public static final String SERVER_NAME = "Noname"; //Votre nom de serveur (IDENTIQUE A CELUI DANS LE LAUNCHER)
+    public static final String APPLICATION_NAME = "Redpack v3"; //nom donné à votre dossier d'installation situé dans %appdata% sur votre PC.
+    public static final String LAUNCHER_URL = "http://maohicraft.crystal-serv.com/launcherbeta/launcher.jar"; //Lien vers votre ficher launcher.jar situé dans votre serveur FTP.
+}
